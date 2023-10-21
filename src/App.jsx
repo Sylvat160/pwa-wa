5f42cea764f8c05af29f7053d710a1fab3634337
Sylvain T <sylvaintagnabou@gmail.com>
2023-10-21 16:16:02 +0000

    (dont) -> with the front and api request

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { fetchWeather } from "./api/fetchWeather";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      setWeather(data);
+      console.log(data);
+      setQuery("");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+
+      {weather.location && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.location.name}</span>
+            <sup> {weather.location.country} </sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.current.temp_c)}
+            <sup>&deg;C </sup>
+          </div>
+          <div className="info">
+            <img
+              src={weather.current.condition.icon}
+              alt={weather.current.condition.code}
+              className="city-icon"
+            />
+            <p> { weather.location.tz_id } </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/api/fetchWeather.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_KEY = '2aa634c3f4cb443bad9154656232110';
+
+export const fetchWeather = async (query) => {
+    const URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${query}`;
+    
+     try {
+        const { data } = await axios.get(URL);
+        return data;
+    } catch (error) {
+        // Handle errors here
+        console.error("Error fetching weather data:", error);
+        throw error;
+    }
+}
